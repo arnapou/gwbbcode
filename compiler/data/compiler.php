41bc7929096bbfa11f4c27ceb73a0adcf65085c7
Arnaud Buathier <arnaud.buathier@livingobjects.com>
2017-07-19 19:51:57 +0200

    formatting

--- compiler/data/compiler.php
+++ compiler/data/compiler.php
@@ -12,7 +12,7 @@
 header('Content-type: text/plain; charset=utf-8');
 
 // right check
-if ( !is_writable(dirname(__FILE__)) ) {
+if (!is_writable(dirname(__FILE__))) {
     die('Error: current folder should be writable !');
 }
 
@@ -23,12 +23,12 @@ define('COMPILED_PATH', dirname(__FILE__) . '/compiled');
 define('DATABASE_PATH', dirname(__FILE__) . '/database');
 
 // images folder check
-if ( !is_dir(DATABASE_PATH) ) {
+if (!is_dir(DATABASE_PATH)) {
     die('Error: cannot find database folder !');
 }
 
 // compiled folder check
-if ( !is_dir(COMPILED_PATH) ) {
+if (!is_dir(COMPILED_PATH)) {
     echo "Creating compiled folder ...\n";
     mkdir(COMPILED_PATH, 0777);
 }
@@ -36,9 +36,9 @@ if ( !is_dir(COMPILED_PATH) ) {
 // empty the compiled path folder
 echo "Emptying compiled folder ...\n";
 $files = glob(COMPILED_PATH . '/*');
-if ( is_array($files) ) {
-    foreach ( $files as $file ) {
-        if ( is_file($file) ) {
+if (is_array($files)) {
+    foreach ($files as $file) {
+        if (is_file($file)) {
             unlink($file);
         }
     }
@@ -46,68 +46,71 @@ if ( is_array($files) ) {
 
 // load languages
 $languageFolders = glob(DATABASE_PATH . '/*', GLOB_ONLYDIR);
-if ( !is_array($languageFolders) || empty($languageFolders) ) {
+if (!is_array($languageFolders) || empty($languageFolders)) {
     die('Error: cannot find language folders !');
 }
-$languages = array();
-foreach ( $languageFolders as $languageFolder ) {
+$languages = [];
+foreach ($languageFolders as $languageFolder) {
     $lang = basename($languageFolder);
-    if ( strlen($lang) == 2 ) {
+    if (strlen($lang) == 2) {
         $languages[] = $lang;
     }
 }
-if ( empty($languages) ) {
+if (empty($languages)) {
     die('Error: cannot find language folders !');
 }
 
 // some usefull functions
-function getJsonKeyPair($filename) {
-    if ( !is_file(DATABASE_PATH . '/' . $filename) ) {
+function getJsonKeyPair($filename)
+{
+    if (!is_file(DATABASE_PATH . '/' . $filename)) {
         die('Error: cannot find "' . $filename . '" file !');
     }
     $fh = fopen(DATABASE_PATH . '/' . $filename, 'r');
     echo "Loading $filename ...\n";
-    $data = array();
+    $data      = [];
     $skipFirst = true;
-    while ( !feof($fh) ) {
+    while (!feof($fh)) {
         $row = fgetcsv($fh, 65536, ';', '"');
-        if ( $skipFirst ) {
+        if ($skipFirst) {
             $skipFirst = false;
             continue;
         }
-        if ( empty($row[0]) ) {
+        if (empty($row[0])) {
             continue;
         }
         $data[utf8_encode($row[0])] = utf8_encode($row[1]);
     }
     return json_encode($data);
 }
-function getJsonSkills($filename) {
-    if ( !is_file(DATABASE_PATH . '/' . $filename) ) {
+
+function getJsonSkills($filename)
+{
+    if (!is_file(DATABASE_PATH . '/' . $filename)) {
         die('Error: cannot find "' . $filename . '" file !');
     }
     $fh = fopen(DATABASE_PATH . '/' . $filename, 'r');
     echo "Loading $filename ...\n";
-    $data = array();
+    $data      = [];
     $skipFirst = true;
-    while ( !feof($fh) ) {
+    while (!feof($fh)) {
         $cols = fgetcsv($fh, 65536, ';', '"');
-        if ( $skipFirst ) {
+        if ($skipFirst) {
             $skipFirst = false;
             continue;
         }
-        if ( empty($cols) || count($cols) != 17 ) {
+        if (empty($cols) || count($cols) != 17) {
             continue;
         }
-        $caracs = array();
-        if($cols[10] !== '') $caracs['s'] = $cols[10];
-        if($cols[11] !== '') $caracs['x'] = $cols[11];
-        if($cols[12] !== '') $caracs['g'] = $cols[12];
-        if($cols[13] !== '') $caracs['e'] = $cols[13];
-        if($cols[14] !== '') $caracs['a'] = $cols[14];
-        if($cols[15] !== '') $caracs['c'] = t($cols[15]);
-        if($cols[16] !== '') $caracs['r'] = $cols[16];
-        $row = array(
+        $caracs = [];
+        if ($cols[10] !== '') $caracs['s'] = $cols[10];
+        if ($cols[11] !== '') $caracs['x'] = $cols[11];
+        if ($cols[12] !== '') $caracs['g'] = $cols[12];
+        if ($cols[13] !== '') $caracs['e'] = $cols[13];
+        if ($cols[14] !== '') $caracs['a'] = $cols[14];
+        if ($cols[15] !== '') $caracs['c'] = t($cols[15]);
+        if ($cols[16] !== '') $caracs['r'] = $cols[16];
+        $row            = [
             'p' => utf8_encode($cols[1]),
             'e' => $cols[2] ? 1 : 0,
             'n' => utf8_encode($cols[3]),
@@ -118,28 +121,30 @@ function getJsonSkills($filename) {
             'a' => utf8_encode($cols[8]),
             'b' => utf8_encode($cols[9]),
             'z' => $caracs,
-        );
+        ];
         $data[$cols[0]] = $row;
     }
     return json_encode($data);
 }
-function t($s) {
+
+function t($s)
+{
     $s = str_replace(',', '.', $s);
-    if($s == 0.25) $s = '&frac14;';
-    if($s == 0.5)  $s = '&frac12;';
-    if($s == 0.75) $s = '&frac34;';
+    if ($s == 0.25) $s = '&frac14;';
+    if ($s == 0.5) $s = '&frac12;';
+    if ($s == 0.75) $s = '&frac34;';
     return $s;
 }
 
 // process by language
-foreach ( $languages as $lang ) {
+foreach ($languages as $lang) {
     echo "Processing $lang ...\n";
     $js = "GWConfig.db.skills = " . getJsonSkills($lang . '/skills.csv') . ";\n";
-    $js.= "GWConfig.db.search = " . getJsonKeyPair($lang . '/search.csv') . ";\n";
-    $js.= "GWConfig.db.professions = " . getJsonKeyPair($lang . '/professions.csv') . ";\n";
-    $js.= "GWConfig.db.professionnames = " . getJsonKeyPair($lang . '/professionnames.csv') . ";\n";
-    $js.= "GWConfig.db.professionkeys = " . getJsonKeyPair($lang . '/professionkeys.csv') . ";\n";
-    $js.= "GWConfig.db.attributes = " . getJsonKeyPair($lang . '/attributes.csv') . ";\n";
+    $js .= "GWConfig.db.search = " . getJsonKeyPair($lang . '/search.csv') . ";\n";
+    $js .= "GWConfig.db.professions = " . getJsonKeyPair($lang . '/professions.csv') . ";\n";
+    $js .= "GWConfig.db.professionnames = " . getJsonKeyPair($lang . '/professionnames.csv') . ";\n";
+    $js .= "GWConfig.db.professionkeys = " . getJsonKeyPair($lang . '/professionkeys.csv') . ";\n";
+    $js .= "GWConfig.db.attributes = " . getJsonKeyPair($lang . '/attributes.csv') . ";\n";
 
     echo "Writing $lang.js ...\n";
     file_put_contents(COMPILED_PATH . '/' . $lang . '.js', $js);
--- compiler/skills/compiler.php
+++ compiler/skills/compiler.php
@@ -12,12 +12,12 @@
 header('Content-type: text/plain; charset=utf-8');
 
 // GD check 
-if ( !extension_loaded('gd') ) {
+if (!extension_loaded('gd')) {
     die('Error: you must have GD extension activated !');
 }
 
 // right check
-if ( !is_writable(dirname(__FILE__)) ) {
+if (!is_writable(dirname(__FILE__))) {
     die('Error: current folder should be writable !');
 }
 
@@ -29,12 +29,12 @@ define('IMAGE_PATH', dirname(__FILE__) . '/images');
 define('IMAGE_SIZE', 50);
 
 // images folder check
-if ( !is_dir(IMAGE_PATH) ) {
+if (!is_dir(IMAGE_PATH)) {
     die('Error: cannot find image folder !');
 }
 
 // compiled folder check
-if ( !is_dir(COMPILED_PATH) ) {
+if (!is_dir(COMPILED_PATH)) {
     echo "Creating compiled folder ...\n";
     mkdir(COMPILED_PATH, 0777);
 }
@@ -42,9 +42,9 @@ if ( !is_dir(COMPILED_PATH) ) {
 // empty the compiled path folder
 echo "Emptying compiled folder ...\n";
 $files = glob(COMPILED_PATH . '/*');
-if ( is_array($files) ) {
-    foreach ( $files as $file ) {
-        if ( is_file($file) ) {
+if (is_array($files)) {
+    foreach ($files as $file) {
+        if (is_file($file)) {
             unlink($file);
         }
     }
@@ -52,33 +52,33 @@ if ( is_array($files) ) {
 
 // load list of images
 $images = glob(IMAGE_PATH . '/*.jpg');
-if ( !is_array($images) || empty($images) ) {
+if (!is_array($images) || empty($images)) {
     die('Error: the image folder should not be empty !');
 }
 
 // build sprites
-$sprites = array();
-foreach ( $images as $image ) {
-    $k = floor(basename($image, '.php') / 100);
+$sprites = [];
+foreach ($images as $image) {
+    $k             = floor(basename($image, '.php') / 100);
     $sprites[$k][] = $image;
 }
 ksort($sprites);
 
 // process sprites
 $css = '';
-foreach ( $sprites as $k => $images ) {
+foreach ($sprites as $k => $images) {
     echo "Building sprite #$k ...\n";
-    $css .= ".skillPack" . $k . "{background-image:url(skills_" . $k . ".jpg);}\n";
+    $css       .= ".skillPack" . $k . "{background-image:url(skills_" . $k . ".jpg);}\n";
     $imgsprite = imagecreatetruecolor(10 * IMAGE_SIZE, ceil(count($images) / 10) * IMAGE_SIZE);
-    $x = 0;
-    $y = 0;
-    foreach ( $images as $filename ) {
-        $css .= ".skill" . basename($filename, '.jpg') . "{background-position:" . (- $x) . "px " . (- $y) . "px;}\n";
+    $x         = 0;
+    $y         = 0;
+    foreach ($images as $filename) {
+        $css      .= ".skill" . basename($filename, '.jpg') . "{background-position:" . (-$x) . "px " . (-$y) . "px;}\n";
         $imgskill = imagecreatefromjpeg($filename);
         imagecopy($imgsprite, $imgskill, $x, $y, 0, 0, IMAGE_SIZE, IMAGE_SIZE);
         imagedestroy($imgskill);
         $x += IMAGE_SIZE;
-        if ( $x >= 10 * IMAGE_SIZE ) {
+        if ($x >= 10 * IMAGE_SIZE) {
             $x = 0;
             $y += IMAGE_SIZE;
         }
